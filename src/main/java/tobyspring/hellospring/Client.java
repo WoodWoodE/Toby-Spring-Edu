a2caf9dd05406667d76f37ff4baf2a2315070e92
WoodWood <wrkmandng@gmail.com>
2024-09-15 20:21:13 +0900

    스타벅스 저녁에 - 원칙과 패턴 중간 커밋

--- src/main/java/tobyspring/hellospring/Client.java
+++ src/main/java/tobyspring/hellospring/Client.java
@@ -5,7 +5,8 @@ import java.math.BigDecimal;
 
 public class Client {
     public static void main(String[] args) throws IOException {
-        PaymentService paymentService = new PaymentService();
+        ObjectFactory objectFactory = new ObjectFactory();
+        PaymentService paymentService = objectFactory.paymentService();
         Payment prepare = paymentService.prepare(1L, "USD", BigDecimal.valueOf(50.7));
         System.out.println(prepare);
     }
--- src/main/java/tobyspring/hellospring/PaymentService.java
+++ src/main/java/tobyspring/hellospring/PaymentService.java
@@ -7,8 +7,9 @@ import java.time.LocalDateTime;
 public class PaymentService {
 
     private final ExRateProvider exRateProvider ;
-    public PaymentService() {
-        this.exRateProvider = new SimpleExRateProvider();
+
+    public PaymentService(ExRateProvider exRateProvider) {
+        this.exRateProvider = exRateProvider;
      }
 
     public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount) throws IOException {
@@ -17,11 +18,11 @@ public class PaymentService {
         LocalDateTime validUntil = LocalDateTime.now().plusMinutes(30);
 
         return new Payment(
-                orderId, 
-                currency, 
-                foreignCurrencyAmount, 
-                exRate, 
-                convertedAmount, 
+                orderId,
+                currency,
+                foreignCurrencyAmount,
+                exRate,
+                convertedAmount,
                 validUntil
         );
     }
