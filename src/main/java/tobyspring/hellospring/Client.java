5b3b6078cfd28b084b0d0d72429060aec547f9a4
WoodWood <wrkmandng@gmail.com>
2024-09-16 17:57:54 +0900

    스타벅스(마천) 점심에 - 싱글톤 레지스트리 종료

--- src/main/java/tobyspring/hellospring/Client.java
+++ src/main/java/tobyspring/hellospring/Client.java
@@ -10,6 +10,10 @@ public class Client {
     public static void main(String[] args) throws IOException {
         BeanFactory beanFactory = new AnnotationConfigApplicationContext(ObjectFactory.class);
         PaymentService paymentService = beanFactory.getBean(PaymentService.class);
+        OrderService orderService = beanFactory.getBean(OrderService.class);
+
+        System.out.println(paymentService.exRateProvider == orderService.exRateProvider);
+
         Payment prepare = paymentService.prepare(1L, "USD", BigDecimal.valueOf(50.7));
         System.out.println(prepare);
     }
