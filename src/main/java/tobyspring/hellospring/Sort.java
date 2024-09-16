fbca7946dffdc7de52175c70589ef8fded59374f
WoodWood <wrkmandng@gmail.com>
2024-09-16 17:20:18 +0900

    스타벅스(마천) 점심에 - 구성정보를 가져오는 다른 방법 종료

---
+++ src/main/java/tobyspring/hellospring/Sort.java
@@ -0,0 +1,14 @@
+package tobyspring.hellospring;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class Sort {
+    public static void main(String[] args) {
+        List<String> scores = Arrays.asList("z", "x", "spring", "java");
+        Collections.sort(scores, (o1, o2) -> o2.length()- o1.length());
+        System.out.println(scores.toString());
+    }
+}
